88a2cf9376dd63a57a860bc3c05823d90c781320
alexmedve <medve_a@yahoo.com>
2019-03-30 00:26:12 +0200

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <div class="nav-wrap green">
+      <div class="container">
+        ASD
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: 'navbar',
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+  h1 {
+    text-align: center;
+  }
+</style>
